66ddbca8f97f5898a5dfe67b02a1f56352facab8
EluanDias <eluan.dias@hotmail.com>
2017-01-12 20:12:07 -0200

    adicionado conteudo portfolio

--- js/index.js
+++ js/index.js
@@ -2,35 +2,36 @@
 
 
 
-// menu botoes  
+
+// menu botoes
 
 $(".menu-abrir").click(function(){
-   
+
    $("html").addClass("menu-ativo");
 
 });
-  
+
   $(".menu-fechar").click(function(){
      $("html").removeClass("menu-ativo");
-  
-     
+
+
 });
-       
+
           $("html").click(function(event) {
-          
-            if(event.target === this) { 
+
+            if(event.target === this) {
           $("html").removeClass("menu-ativo");
         }
-      });    
+      });
 
 /*display*/
 
 /*$(".header-portfolio").click(function(){
-        $(".portfolio ").toggle(1000);  
+        $(".portfolio ").toggle(1000);
 });
 
 $(".sobre").click(function(){
-        $(".row").toggle(1000);  
+        $(".row").toggle(1000);
 });
 
 
@@ -40,10 +41,16 @@ window.setTimeout(myFunction, 3000);
       //  $(".row").hide(1000);
         //     $(".conteudo").hide(1000);
 }*/
- 
-        
-  
 
+
+
+
+// -----inicializa tooltips from icones
+$(document).ready(function(){
+$('[data-toggle="tooltip"]').tooltip({
+placement : 'top'
+});
+});
 
 
 //----------
@@ -53,11 +60,11 @@ window.setTimeout(myFunction, 3000);
      $('html').addClass('item_open');
     var itemID = $(this).attr('href');
      $(itemID).addClass('item_open');
-   
+
  });
  $('.close').click(function() {
      $('.port, .gallery ul').removeClass('item_open');
-    
+
  });
 
  $(".gallery ul li a").click(function() {
@@ -67,41 +74,41 @@ window.setTimeout(myFunction, 3000);
  });
 
 // --------------scroll para o  top
-     
+
 var scrollTop = $(".scrollTop");
 
 $(window).scroll(function(){
     var posicao = $(this).scrollTop();
-  
+
   if( posicao >600 ){
     $(scrollTop).css("opacity", 1);
   } else{
     $(scrollTop).css("opacity", 0);
-  } 
-       
-    
+  }
+
+
 });
 
 // animando scrolltop
-                   
+
   $(".sobre").click(function() {
     $('html, body').animate({
       scrollTop: 0
     }, 800);
-                   
- });                  
+
+ });
 
 $(".header-portfolio").click(function() {
     $('html, body').animate({
       scrollTop: 800
-    }, 800);            
+    }, 800);
  });
 
 
  $(".contato").click(function() {
     $('html, body').animate({
       scrollTop: 1600
-    }, 800);                
+    }, 800);
  });
 
  $(scrollTop).click(function() {
@@ -115,24 +122,24 @@ $(".header-portfolio").click(function() {
 
 $(document).ready(function(){
   $(window).scroll(function() {
-    
+
     if ($(document).scrollTop() < 600) {
       $(".li1").addClass("test");
     } else {
       $(".li1").removeClass("test");
     };
-    
+
       if (($(document).scrollTop() > 600) && ($(document).scrollTop() < 1200)) {
       $(".li2").addClass("test");
     } else {
       $(".li2").removeClass("test");
     };
-    
+
     if ($(document).scrollTop() > 1300) {
       $(".li3").addClass("test");
     } else {
       $(".li3").removeClass("test");
     };
-    
+
   });
 });
